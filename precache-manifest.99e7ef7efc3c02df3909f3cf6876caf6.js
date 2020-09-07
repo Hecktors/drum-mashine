2adc3e8c12a5243ba356906d3bc736ed9172f320
hecktors <tohe.webdev@gmail.com>
2020-09-07 12:58:46 +0200

    Updates

---
+++ precache-manifest.99e7ef7efc3c02df3909f3cf6876caf6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac391e7391882d1aeaeedafdd141fca",
+    "url": "/drum-mashine/index.html"
+  },
+  {
+    "revision": "71789329e0a64b606aa6",
+    "url": "/drum-mashine/static/css/main.0535f3b8.chunk.css"
+  },
+  {
+    "revision": "e2bf9c4b0ff4f7da8c9d",
+    "url": "/drum-mashine/static/js/2.9bce78eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-mashine/static/js/2.9bce78eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71789329e0a64b606aa6",
+    "url": "/drum-mashine/static/js/main.b2ea045c.chunk.js"
+  },
+  {
+    "revision": "b363cc9f34d582f761ef",
+    "url": "/drum-mashine/static/js/runtime-main.f9c51016.js"
+  },
+  {
+    "revision": "54c479f440e052946da3db982ba5514e",
+    "url": "/drum-mashine/static/media/agogo.54c479f4.mp3"
+  },
+  {
+    "revision": "324549ec5f36dab5407fa818dcc25925",
+    "url": "/drum-mashine/static/media/backspace.324549ec.svg"
+  },
+  {
+    "revision": "4d84bea026bdf49aa80f4f7db9b3a053",
+    "url": "/drum-mashine/static/media/cabasa.4d84bea0.mp3"
+  },
+  {
+    "revision": "7e97193116edfcbc6bc82ff3806d3a37",
+    "url": "/drum-mashine/static/media/conga.7e971931.mp3"
+  },
+  {
+    "revision": "3b39781f284798d52e35ec107e309894",
+    "url": "/drum-mashine/static/media/conga2.3b39781f.mp3"
+  },
+  {
+    "revision": "db68930fac5d69f6b24b24b15ee92a53",
+    "url": "/drum-mashine/static/media/conga_clean.db68930f.mp3"
+  },
+  {
+    "revision": "795e39d466f888cb8bf3c2659f2aa61f",
+    "url": "/drum-mashine/static/media/conga_clean_higher.795e39d4.mp3"
+  },
+  {
+    "revision": "5016cc38539dc6d5b5a521cebda05639",
+    "url": "/drum-mashine/static/media/conga_clean_low.5016cc38.mp3"
+  },
+  {
+    "revision": "35f9510aacb1cd0f6ce58e1bde528510",
+    "url": "/drum-mashine/static/media/conga_clean_lower.35f9510a.mp3"
+  },
+  {
+    "revision": "e3e7453214defa0310b91725ae29c933",
+    "url": "/drum-mashine/static/media/conga_clean_lowest.e3e74532.mp3"
+  },
+  {
+    "revision": "7636a58bef95279e71771de09bc43433",
+    "url": "/drum-mashine/static/media/delete.7636a58b.svg"
+  },
+  {
+    "revision": "108cbd14f87e9a4806dea1ef71c39835",
+    "url": "/drum-mashine/static/media/play.108cbd14.svg"
+  }
+]);
